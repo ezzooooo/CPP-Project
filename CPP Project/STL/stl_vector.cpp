12aec24c39588b86892632f154b63a584ec37f73
LeeJuWon <dkreka11@gmail.com>
2020-01-21 19:46:39 +0900

    stl 벡터, 이터레이터

---
+++ CPP Project/STL/stl_vector.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <vector>
+//
+//int main() {
+//  std::vector<int> vec;
+//  vec.push_back(10);    // 맨 뒤에 10 추가
+//  vec.push_back(20);    // 맨 뒤에 20 추가
+//  vec.push_back(30);    // 맨 뒤에 30 추가
+//  vec.push_back(40);    // 맨 뒤에 40 추가
+//
+//  for (int i = 0; i < 4; i++) {
+//    std::cout << "vec 의 " << i + 1 << " 번째 원소 :: " << vec.at(vec.size()-1) << std::endl;
+//    vec.pop_back();
+//  }
+//}

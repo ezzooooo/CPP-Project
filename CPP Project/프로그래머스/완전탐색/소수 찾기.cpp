685750a9a53c19ac1367e158e053c83874df5746
LeeJuWon <dkreka11@gmail.com>
2020-03-10 17:04:09 +0900

    소수찾기 해결!

--- CPP Project/프로그래머스/완전탐색/소수 찾기.cpp
+++ CPP Project/프로그래머스/완전탐색/소수 찾기.cpp
@@ -1,85 +1,70 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-#include <set>
-#define MAX 9999999999
-using namespace std;
-
-bool isPrime(int number)
-{
-  if (number == 1)
-    return false;
-  if (number == 2)
-    return true;
-  if (number % 2 == 0)
-    return false;
-
-  bool isPrime = true;
-  for (int i = 2; i <= sqrt(number); i++)
-  {
-    if (number% i == 0)
-      return false;
-  }
-
-  return isPrime;
-}
-
-bool compare(char a, char b)
-{
-  return a > b;
-}
-
-int solution(string numbers) {
-  int answer = 0;
-
-  string temp;
-  temp = numbers;
-
-  sort(temp.begin(), temp.end(), compare);
-
-  vector<bool> prime(std::stoi(temp) + 1);
-
-  cout << stoi(temp) << endl;
-  prime[0] = false;
-  for (long long i = 1; i < prime.size(); i++)
-  {
-    prime[i] = isPrime(i);
-  }
-  //cout << "chk1" << endl;
-  //int num = std::stoi(numbers);
-
-  string s, sub;
-
-  s = numbers;
-
-  sort(s.begin(), s.end());
-  set<int> primes;
-  int l = s.size();
-  do {
-    for (int i = 1; i <= l; i++)
-    {
-      sub = s.substr(0, i);
-      //  cout << "chk2" <<  " " << sub<<  endl;
-      if (prime[std::stoi(sub)])
-      {
-        primes.insert(std::stoi(sub));
-      }
-    }
-  } while (next_permutation(s.begin(), s.end()));
-
-  //cout << primes.size();
-  answer = primes.size();
-  return answer;
-}
-
-int main() {
-
-  string numbers = "111";
-
-  int a = solution(numbers);
-
-  cout << a << endl;
-
-  system("pause");
-}
+//// 프로그래머스 완전탐색 소수찾기
+//
+//#include <string>
+//#include <vector>
+//#include <iostream>
+//#include <math.h>
+//#include <algorithm>
+//#include <set>
+//
+//using namespace std;
+//
+//bool is_prime(int num) {
+//  if (num == 1) return false;
+//  if (num == 2) return true;
+//
+//  for (int i = 2; i <= sqrt(num); i++) {
+//    if (num % i == 0) {
+//      return false;
+//    }
+//  }
+//  return true;
+//}
+//
+//bool compare(char a, char b) {
+//  return a > b;
+//}
+//
+//int solution(string numbers) {
+//  int answer = 0;
+//  string temp = numbers;
+//  sort(temp.begin(), temp.end(), compare);
+//
+//  vector<bool> prime(stoi(temp) + 1);
+//  prime[0] = false;
+//
+//  for (int i = 1; i < prime.size(); i++) {
+//    prime[i] = is_prime(i);
+//  }
+//
+//  set<int> primes;
+//  int l = numbers.size();
+//
+//  do {
+//    for (int i = 1; i <= l; i++) {
+//      temp = numbers.substr(0, i);
+//      cout << temp << endl;
+//
+//      if (prime[stoi(temp)]) {
+//        primes.insert(stoi(temp));
+//      }
+//    }
+//
+//
+//  } while (next_permutation(numbers.begin(), numbers.end()));
+//
+//
+//
+//  return primes.size();
+//}
+//
+//int main() {
+//
+//  string numbers = "013";
+//
+//  int a = solution(numbers);
+//
+//  cout << a << endl;
+//
+//  system("pause");
+//}

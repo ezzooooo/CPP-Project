76eab2a99266d25e9db95c69d7006de11d21c81b
LeeJuWon <dkreka11@gmail.com>
2020-03-27 22:22:38 +0900

    가운데를 말해요 해결!

--- CPP Project/알고리즘챌린지/가운데를 말해요.cpp
+++ CPP Project/알고리즘챌린지/가운데를 말해요.cpp
@@ -4,47 +4,47 @@
 //
 //using namespace std;
 //
+//priority_queue<int> max_heap;
+//priority_queue<int, vector<int>, greater<int>> min_heap;
+//
 //int main() {
 //
 //  int n;
-//
-//  cin >> n;
-//
-//  vector<int> nums(n);
-//  vector<int> say_nums(n);
-//  priority_queue<int> max_heap;
-//  priority_queue<int, vector<int>, greater<int>> min_heap;
+//  scanf("%d", &n);
 //
 //  for (int i = 0; i < n; i++) {
-//    cin >> nums[i];
-//  }
+//    int input;
+//    scanf("%d", &input);
 //
-//  max_heap.push(nums[0]);
-//  say_nums.push_back(nums[0]);
-//
-//  for (int i = 1; i < n; i++) {
-//
-//    if (nums[i] < max_heap.top()) {
-//      max_heap.push(nums[i]);
+//    if (max_heap.size() == min_heap.size()) {
+//      if (max_heap.size() == 0 && min_heap.size() == 0) {
+//        max_heap.push(input);
+//      }
+//      else {
+//        if (min_heap.top() < input) {
+//          int temp = min_heap.top();
+//          min_heap.pop();
+//          min_heap.push(input);
+//          max_heap.push(temp);
+//        }
+//        else {
+//          max_heap.push(input);
+//        }
+//      }
 //    }
 //    else {
-//      min_heap.push(nums[i]);
-//    }
-//    
-//    if (max_heap.size() < min_heap.size()) {
-//      max_heap.push(min_heap.top());
-//      min_heap.pop();
-//    }
-//    else if (max_heap.size() > min_heap.size() + 1) {
-//      min_heap.push(max_heap.top());
-//      max_heap.pop();
+//      if (max_heap.top() > input) {
+//        int temp = max_heap.top();
+//        max_heap.pop();
+//        max_heap.push(input);
+//        min_heap.push(temp);
+//      }
+//      else {
+//        min_heap.push(input);
+//      }
 //    }
 //
-//    say_nums[i] = max_heap.top();
-//  }
-//
-//  for (int i = 0; i < n; i++) {
-//    cout << say_nums[i] << endl;
+//    printf("%d\n", max_heap.top());
 //  }
 //
 //  return 0;
